a3a22d35adb363d7780021f1b758871c420a5d43
David Gaona <davidgaonahenao@gmail.com>
2024-02-15 10:49:16 -0500

    Send gifs and trends completed

---
+++ app.js
@@ -0,0 +1,48 @@
+const apiKey = "1Yr6NC6BgkDbgE4SHeLgfOx6YD29HANq";
+const searchInput = document.getElementById("search-bar");
+const gifContainer = document.getElementById("gif-container");
+
+searchInput.addEventListener("input", mustunce(searchGifs, 300));
+
+async function searchGifs() {
+  const searchTerm = searchInput.value.trim();
+
+  if (searchTerm === "") {
+    gifContainer.innerHTML = ""; 
+    return;
+  }
+
+  try {
+    const endpoint = `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${apiKey}&limit=10`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    showGifs(data.data);
+  } catch (error) {
+    console.error("Error to search gifs:", error);
+  }
+}
+
+function showGifs(gifs) {
+  gifContainer.innerHTML = "";
+
+  gifs.forEach((gif) => {
+    const img = document.createElement("img");
+    img.src = gif.images.fixed_height.url;
+    img.classList.add("gif");
+    gifContainer.appendChild(img);
+  });
+}
+
+
+function mustunce(func, delay) {
+  let timeoutId;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
